01c7c4f1dcb95fbe944e68aacdd4f008faee0d3d
Rebanso <rebansochart@gmail.com>
2025-08-06 11:00:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
-if(file_exists(__DIR__ . '/classes/account.php')){
-    require_once __DIR__ . '/classes/account.php';
+
+if(file_exists(__DIR__ . '/Classes/Account.php')){
+    require_once __DIR__ . '/Classes/Account.php';
     $Account = new Account();
-}
+};
 
-if(file_exists(__DIR__ . '/classes/bank.php')){
-    require_once __DIR__ . '/classes/bank.php';
+if(file_exists(__DIR__ . '/Classes/Bank.php')){
+    require_once __DIR__ . '/Classes/Bank.php';
     $Bank = new Bank();
-}
-
+};
+if(file_exists(__DIR__ . '/Models/User.php')){
+    require_once __DIR__ . '/Models/User.php';
+    $User = new User();
+};
